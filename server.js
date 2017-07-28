e0231a1e8327ca31230edb8fcd3976a1935c74e1
tanzeelak <tnkhan8042@gmail.com>
2017-07-27 22:33:02 -0700

    starting from working navbar

---
+++ server.js
@@ -0,0 +1,74 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+var path = __dirname + '/views/';
+var hbs = require('hbs');
+
+app.set('view engine', 'hbs');
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
+
+router.get("/",function(req,res){
+    res.render('index');
+});
+
+router.get("/team",function(req,res){
+    res.render('team');
+});
+
+router.get("/families",function(req,res){
+    res.render('families');
+});
+router.get("/join",function(req,res){
+    res.render('join');
+});
+router.get("/contact",function(req,res){
+    res.render('contact');
+});
+router.get("/calendar",function(req,res){
+    res.render('calendar');
+});
+router.get("/gallery",function(req,res){
+    res.render('gallery');
+});
+
+router.get("/projects",function(req,res){
+  res.render('projects');
+});
+
+router.get("/sponsors",function(req,res){
+  res.render('sponsors');
+});
+
+router.get("/donate",function(req,res){
+  res.render('donate');
+});
+
+router.get("/memes",function(req,res){
+  res.render('memes');
+});
+
+router.get("/blog",function(req,res){
+  res.render('blog');
+});
+
+router.get("/shop",function(req,res){
+  res.render('shop');
+});
+
+app.use("/",router);
+
+app.use(express.static("public"));
+
+app.use("*",function(req,res){
+    res.render('404');
+});
+
+app.listen(3000,function(){
+  console.log("Live at Port 3000");
+});
