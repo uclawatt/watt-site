926f5d3f6fc6e04e1cf850c2c2a571a31d710131
tanzeelak <tnkhan8042@gmail.com>
2017-08-23 23:40:07 -0700

    before parallax fails

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,23 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
+
+
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            var target = $(this).attr('href');
+            $('.parallax').animate({
+                scrollTop: $(target).offset().top + $('.parallax').scrollTop()
+            }, 2500);
+            return false;
+        });
+    });
+
+    $(document).ready(function () {
+        $(".navbar-nav li a").click(function(event) {
+            $(".navbar-collapse").collapse('hide');
+        });
+    });
+    
     // var user, to, subject, text;
     // $("#send_email").click(function () {
     //     // enter your email account that you want to recieve emails at.
@@ -64,7 +82,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     // $('.parallax').parallax();
 
-    $('.parallax-window').parallax({imageSrc: 'img/group-photo.jpg'});
+    // $('.parallax-window').parallax({imageSrc: 'img/group-photo.jpg'});
 
 
 
