e41dace8eb13badc5b62ee6851aa179ea836f047
Justine-Lin <50601091+Justine-Lin@users.noreply.github.com>
2021-03-02 20:01:15 -0800

    weekly updates

--- public/js/main.js
+++ public/js/main.js
@@ -18,20 +18,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var dataObj = {
     events: [
       {
-        eventUrl: "https://www.facebook.com/events/490170465476758",
-        name: "Python Game Workshop",
-        date: "Week 9 3/1/21",
+        eventUrl: "https://www.facebook.com/events/286064359523165",
+        name: "WATT Study and Game Night",
+        date: "Thurs 3/4/21",
         location: "Zoom",
-        time: "6:00 PM - 8:00 PM PST",
+        time: "8:00 PM - 9:00 PM PST",
         description:
-          "Ever wondered how to code a game in Python? Come join us and work with a friend to code a Snake game in Python! This workshop is beginner friendly, so anyone at any skill level is welcome!"
+          "Come through to WATT's Study/Game night to hang out with us! If you want to rant, game, chat, cry, study, or all of the above, COME THROUGH!"
       },
       {
         eventUrl: "https://www.facebook.com/events/1121401364950413",
         name: "Raspberry Pi Workshop II",
-        date: "Week 9 3/3/21",
+        date: "Wed 3/31/21",
         location: "Zoom",
-        time: "7:00 PM - 8:00 PM PST",
+        time: "6:30 PM - 7:30 PM PST",
         description:
           "This will be the second workshop in our Raspberry Pi Workshop series. We will be setting up the Raspberry Pi's and hopefully getting started with the basics of the project!"
       }
